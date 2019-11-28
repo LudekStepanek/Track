eae403a1d5ce34d2a9c381057ec8906af1046e88
LudekStepanek <56113304+LudekStepanek@users.noreply.github.com>
2019-11-28 17:57:11 +0100

    read/save better

--- main.R
+++ main.R
@@ -4,6 +4,7 @@ plot_folder <- ("C:\\Users\\Ludek\\Documents\\R\\plots\\")
 
 data_folder <- "C:\\Users\\Ludek\\Documents\\R\\data\\tracking_starved\\"
 data_folder <- "C:\\Users\\Ludek\\Documents\\R\\data\\ts_sample\\"
+data_folder <- "C:\\Users\\Ludek\\Documents\\R\\data\\tracking_old\\"
 
 
 data_folder <- "K:\\t_sample\\"
@@ -20,16 +21,31 @@ tracks <- import_tracks(data_folder, Load = F)
 
 add_1(tracks)
 
-add_DP_simplify(tracks, 120)
+add_DP_simplify(tracks, 25)
 
-tracks_old <- readRDS("K:\\TrackingExperimentProcessed\\data_add2.sav")
+
+
+tracks_old <- readRDS(paste0(data_folder,"data_add2.sav"))
 setDT(tracks_old)
 
 tracks_starved <- tracks
 
-tracks_starved <- readRDS("K:\\TrackingExperimentProcessed\\data_add2.sav")
+tracks_starved <- readRDS(paste0(data_folder,"data_add2.sav"))
 setDT(tracks_starved)
 
-add_DP_simplify(tracks, 24)
+add_DP_simplify(tracks, 25)
 print_tracks_by_quantile(tracks_starved)
 
+tracks <- rbind(tracks_old, tracks_starved)
+
+#-------read processed bind to one frame----------
+data_folder <- "C:\\Users\\Ludek\\Documents\\R\\data\\tracking_old\\"
+tracks_old <- readRDS(paste0(data_folder,"data_add2.sav"))
+setDT(tracks_old)
+
+data_folder <- "C:\\Users\\Ludek\\Documents\\R\\data\\tracking_starved\\"
+tracks_starved <- readRDS(paste0(data_folder,"data_add2.sav"))
+setDT(tracks_starved)
+
+
+tracks <- rbind(tracks_old, tracks_starved)
