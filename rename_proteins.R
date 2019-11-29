d49750988986be34fbcd8c39bd46f8e626067356
LudekStepanek <56113304+LudekStepanek@users.noreply.github.com>
2019-11-29 15:39:56 +0100

    Add Strain, Protein and Induced columns to identify the cel line

--- rename_proteins.R
+++ rename_proteins.R
@@ -1,12 +1,12 @@
 
+rename_proteins <- function(tracks, data_folder){
 
-protein_IDs <- fread("C:\\Users\\ludek\\Documents\\R\\Data\\tracking_old\\Proteins_strains_table.csv")
-
+protein_IDs <- fread(paste0(data_folder,"Proteins_strains_table.csv") )
 protein_IDs[,V3:=paste0("strain",V3)]
+tracks[,Strain:=gsub("_.*","",File) ]
+tracks[, `:=`(Protein = protein_IDs[V3 == .BY, V1],
+              Induced = protein_IDs[V3 == .BY, V2]) , by = Strain] 
 
-tracks_old[,Strain:=gsub("_.*","",File) ]
-
-tracks_old[, `:=`(Protein = protein_IDs[V3 == .BY, V1],
-                  Induced = protein_IDs[V3 == .BY, V2]) , by = Strain] 
+}
 
 
