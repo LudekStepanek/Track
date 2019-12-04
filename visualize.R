903f79dcfc068f6cadf192b8761af157681420a5
LudekStepanek <56113304+LudekStepanek@users.noreply.github.com>
2019-12-04 12:48:39 +0100

    Changed to unlist, added Line/IND grouping

--- visualize.R
+++ visualize.R
@@ -5,31 +5,25 @@ print_tracks_by_quantile <- function(tracks, quant = 0.01){
 list_of_plots <- tracks[n>50,
                .(x=x,
                  y=y,
-                 DP_simplify=DP_simplify,
-                 Track = .I,
-                 Shape = lapply(DP_simplify, function(x){sum(x)})
-                
-               )
+                 Shape = dps[Track == .BY, sum(DP_simplify)]
+               ),
+               by = Track
                ][,
-                 `:=`(bin_shape=findInterval(unlist(Shape), 
-                                             quantile(unlist(Shape),probs = seq(0, 1, quant),
+                 `:=`(bin_shape=findInterval(Shape, 
+                                             quantile(Shape, probs = seq(0, 1, quant),
                                                       type=7),
                                              rightmost.closed=TRUE)
                  )
                  ][,
-                    .(p = list(ggplot(.SD[,
-                                          .(x = unlist(x),
-                                            y = unlist(y)
-                                          ),
-                                          by = .(Track)],
-                                      aes(x ,y ,group = Track) )+
-                                 geom_path()+
+                    .(p = list(ggplot(.SD,
+                                      aes(x ,y ,group = Track)) +
+                                 geom_path() +
                                  coord_fixed()
                                
                     ),
                     DP = list(ggplot(.SD[,
-                                         .(x = rep(1:24),
-                                           y = unlist(DP_simplify)
+                                         .(x = rep(1:25),
+                                           y = dps[Track == .BY, DP_simplify]
                                          ),
                                          by = .(Track)],
                                      aes(x ,y ,group = Track) )+
@@ -43,38 +37,36 @@ list_of_plots <- tracks[n>50,
   ggsave(paste0(plot_folder,"_DP_scan1.pdf"),
          marrangeGrob(grobs = list_of_plots$p, nrow = 1, ncol = 1),
          width = 20, height = 20, units = "cm"
-  )
+          )
 
   ggsave(paste0(plot_folder,"DP_scan_DP1.pdf"),
          marrangeGrob(grobs = list_of_plots$DP, nrow = 1, ncol = 1),
          width = 20, height = 20, units = "cm"
-  )
+         )
 }
 
 #---------plot marginal histograms by starved/fresh-----------
 plot_marginals <- function(tracks){
   
-list_of_plots <- tracks[sapply(t ,`[[`, 1) < 5,
-                         .(Shape = sapply(DP_simplify, sum),
-                           Speed = sapply(Speed_res, mean),
-                           n = .N
+list_of_plots <- tracks[,
+                         .(
+                           Shape = dps[Track == .BY, sum(DP_simplify)],
+                           Speed = spt[Track == .BY, mean(Speed)],
+                           Induced = Induced,
+                           Line = Line
                            ),
-                         by = .(Group = fifelse(File%ilike%"load", "new", "old"))
+                         by = Track 
                          ][,
                            `:=`(
-                           Shape = Shape/max(Shape),
-                           n = min(n)
+                           Shape = Shape/max(Shape)
                            )
                          ][,
-                           .SD[sample(.N,n)],
-                           by = Group
-                         ][,
-                           .(plot = list( marginal_histogram(.SD[, .(Shape, Speed, Group)],
-                                                             .SD[, .(Y = .GRP, titulek = "ind", n=n),
-                                                                 by = Group] 
+                           .(plot = list( marginal_histogram(.SD[, .(Shape, Speed, Induced)],
+                                                             .SD[, .(Y = .GRP, titulek = Line, n="x"), by = .(Group = Induced)] 
                                                              )
                                           )
-                             )
+                             ),
+                           by = Line
                            ]
 
 
