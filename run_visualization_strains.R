6088998a02a08dd239f2e71d54f523b6c01ea7b5
LudekStepanek <56113304+LudekStepanek@users.noreply.github.com>
2020-01-15 15:03:42 +0100

    order pages in the pdf file by speed difference

--- run_visualization_strains.R
+++ run_visualization_strains.R
@@ -1,4 +1,6 @@
-plot_data_marginals <- tracks[ n > 60 & Experiment %in% c("2019_12_02", "2019_12_05"),
+globalspeed <- tracks[Experiment%in%c("2019_12_02","2019_12_05") & Condition == "NON", mean(unlist(Speed))]
+
+plot_data_marginals <- tracks[ n > 60 & Experiment %in% c("2019_12_02","2019_12_05"),
                     .(Shape = sapply(DP_simplify, sum),
                       Speed = sapply(Speed, function(x) (mean(x))),
                       Control = Condition %in% "NON",
@@ -24,11 +26,16 @@ plot_data_marginals <- tracks[ n > 60 & Experiment %in% c("2019_12_02", "2019_12
                         Max_Speed = 20,
                         rnd = sample(.N, .N)
                       )
-                      ]
+                      ][,
+                        `:=`(
+                          rank = .SD[Condition == "IND",mean(unlist(Speed))]- globalspeed
+                        ),
+                        by = Line
+                        ]
 
-setorder(plot_data_marginals,-rnd)
+setorder(plot_data_marginals,rank,-rnd) #rank - have the pages in the plot ordered by amount of difference
 
-plot_marginals(plot_data_marginals, "marg_strains.pdf")
+plot_marginals(plot_data_marginals, "marg_new_strains.pdf")
 
 print_tracks_by_quantile(tracks[ n > 200 & Experiment %in% c("2019_12_02")])
 
